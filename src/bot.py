629f01e73595af7f2c3865c6a0eb84a5f57474d8
akakou <s16003@tokyo.kosen-ac.jp>
2019-02-22 15:32:34 +0900

    ユーザに対するリプライを取得

--- src/bot.py
+++ src/bot.py
@@ -8,10 +8,12 @@ auth = tweepy.OAuthHandler(key.consumer_key, key.consumer_secret)
 auth.set_access_token(key.access_token_key, key.access_token_secret)
 api = tweepy.API(auth)
 
+query = ["to:akakou_py"]
+
 # 検索
-for status in api.search(q='"アイカツ"', lang='ja', result_type='recent',count=100):
+for status in api.search(q=query, lang='ja', result_type='recent'):
     print("-*-*-*-*-")
-    print(f"name: {status.user.name}\n")    # ユーザ名を表示
+    print(f"name: {status.user.screen_name}\n")    # ユーザ名を表示
     print(f"text: {status.text}\n")         # ツイートの内容を表示
     print(f"time: {status.created_at + datetime.timedelta(hours=9)}\n") # 時間を表示
     print("-*-*-*-*-\n\n")
