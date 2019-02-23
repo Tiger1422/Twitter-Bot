51c222080b0ae4961fe2413ae330a34abdc5b54b
akakou <s16003@tokyo.kosen-ac.jp>
2019-02-23 17:06:39 +0900

    新しいメッセージと古いメッセージを分離させた

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,6 @@
 import tweepy
 import pprint
-import datetime
+from datetime import datetime, timedelta
 import key
 
 # Twitterに接続＆認証
@@ -15,8 +15,18 @@ for status in api.search(q=query, lang='ja', result_type='recent'):
     print("-*-*-*-*-")
     print(f"name: {status.user.screen_name}\n")    # ユーザ名を表示
     print(f"text: {status.text}\n")         # ツイートの内容を表示
-    print(f"time: {status.created_at + datetime.timedelta(hours=9)}\n") # 時間を表示
     
+    tweet_day = status.created_at + timedelta(hours=9)
+    
+    today = datetime.today()
+    a_hour_ago = today - timedelta(hours=1)
+    print(f"time: {tweet_day}\n") # 時間を表示
+    
+    if tweet_day < a_hour_ago:
+        print("message: pass")
+        print("-*-*-*-*-\n\n")
+        continue
+
     id = status._json['id']
     print(f"id: {id}\n")
     url = f"https://twitter.com/{status.user.screen_name}/status/{id}"
