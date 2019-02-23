8baa9756b21a27a5de3127f7bd6563335037c8c1
akakou <s16003@tokyo.kosen-ac.jp>
2019-02-23 14:24:59 +0900

    URLの取得

--- src/bot.py
+++ src/bot.py
@@ -16,6 +16,10 @@ for status in api.search(q=query, lang='ja', result_type='recent'):
     print(f"name: {status.user.screen_name}\n")    # ユーザ名を表示
     print(f"text: {status.text}\n")         # ツイートの内容を表示
     print(f"time: {status.created_at + datetime.timedelta(hours=9)}\n") # 時間を表示
+    
+    id = status._json['id']
+    print(f"id: {id}\n")
+    url = f"https://twitter.com/{status.user.screen_name}/status/{id}"
+    print(f"url: {url}")
+
     print("-*-*-*-*-\n\n")
-
-
